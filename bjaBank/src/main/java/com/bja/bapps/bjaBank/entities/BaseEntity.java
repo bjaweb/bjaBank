5519aa3a0a24bff74697cff79352b38bc1810262
Bernard JANVION <bjanvion@gmail.com>
2016-05-12 03:12:29 +0200

    base pour les entity

---
+++ bjaBank/src/main/java/com/bja/bapps/bjaBank/entities/BaseEntity.java
@@ -0,0 +1,9 @@
+package com.bja.bapps.bjaBank.entities;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BaseEntity {
+
+		protected static Logger log = LoggerFactory.getLogger(BaseEntity.class);
+}
